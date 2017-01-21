56dde674ac57291ada01616ad52908a35cd6e21a
Aakil Fernandes <aakilfernandes@gmail.com>
2017-01-21 15:59:49 -0500

    add name to message; fix stack; set name property

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function createTestableErrorClass(name) {
   function TestableError(message){
     this.message = `${this.name}: ${message}`
-    const last_part = new Error().stack.match(/[^\s]+$/)
-    this.stack = `${this.name} at ${last_part}: ${this.message}`
+    this.stack = new Error().stack
 
   }
   TestableError.prototype  = Object.create(Error.prototype)
   TestableError.prototype.name = name
   TestableError.prototype.constructor = TestableError
+  Object.defineProperty(TestableError, 'name', { value: name })
   return TestableError
 }
